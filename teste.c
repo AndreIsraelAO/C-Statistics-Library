c94bd21e1e21e39d3909d754b94981930607d103
AndreIsraelAO <andreisraelandalecio.o@gmail.com>
2025-12-06 11:07:55 -0300

    Add files via upload

---
+++ teste.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "estatistics.h"
+
+int main() {
+    double numeros[] = {1, 2, 2, 3, 4};
+    int tamanho = 5;
+
+    printf("Média: %.2f\n", media(numeros, tamanho));
+    printf("Mediana: %.2f\n", mediana(numeros, tamanho));
+    printf("Moda: %.2f\n", moda(numeros, tamanho));
+
+    return 0;
+}
